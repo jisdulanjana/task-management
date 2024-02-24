28d1e8c0603054455212c5e119639ac0a591f32f
Ishan S Dulanjana <jisdulanjana@gmail.com>
2024-02-24 18:39:28 +0530

    add sate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState({
+    title: '',
+    date: '',
+    statuss: '',
+    priority: '',
+});
+
+console.log(tasks);
+
+const handleOnChange = (e) => {
+  setTasks({
+    ...tasks,
+    [e.target.name]: e.target.value
+  });
+}
   return (
-    <div className="App text-blue-400">
-      Text
+    <div className="App">
+      <div className='h-screen w-auto bg-red-300'>
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col'>
+            <label>First Name</label>
+            <input 
+            name='title' 
+            onChange={(e) => handleOnChange(e)} 
+            value={tasks.title}
+            placeholder='task'
+            />
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
